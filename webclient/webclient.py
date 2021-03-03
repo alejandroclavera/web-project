41e2fd3d53da1d3e27f9168bd977d01749057d3a
alejandroclavera <alejandroclavera@gmail.com>
2021-03-03 15:13:31 +0100

    El cliente obtiene lista de noticias

---
+++ webclient/webclient.py
@@ -0,0 +1,31 @@
+from urllib.request import urlopen
+import bs4
+
+class WebClient(object):
+
+    def __init__(self):
+        pass
+
+    def get_web_page(self):
+        # Get web page
+        webpage = urlopen('http://bid.udl.cat/ca/')
+        html = webpage.read()
+        return html
+    
+    def parse_web_page(self, html):
+        soup = bs4.BeautifulSoup(html, features='lxml')
+        news = soup.find_all('li', 'box')
+        return news
+
+    def get_information(self):
+        html = self.get_web_page()
+        info = self.parse_web_page(html)
+        # read information
+        return info
+
+if __name__ == '__main__':
+    client = WebClient()
+    information = client.get_information()
+    print(information)
+
+    
